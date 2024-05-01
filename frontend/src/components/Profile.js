9496e51f4c0d0d6179e12cd8e73dfb4c20c38c0c
ledangxuanbach <20130199@st.hcmuaf.edu.vn>
2024-05-01 23:50:23 +0700

    use formik for update info user

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,20 +1,33 @@
 import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import ".././css/profileStyle.css";
 import { deleteToken } from "../js/redux/actions";
 import { useDispatch } from "react-redux";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const UpdateProfileSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Vui lòng nhập đúng định dạng email")
+    .required("Vui lòng không để trống"),
+  name: Yup.string().required("Vui lòng không để trống"),
+  phoneNumber: Yup.string()
+    .matches(/^[0-9]+$/, "Số điện thoại phải là số")
+    .min(10, "Số điện thoại không hợp lệ")
+    .required("Vui lòng nhập số điện thoại của bạn"),
+});
 
 const Profile = () => {
   const token = useSelector((state) => state.token);
   const [accountInfor, setAccountInfor] = useState(null);
   const [loading, setLoadings] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log("loading data...");
     if (token) {
       setLoadings(true);
       axios
@@ -43,15 +56,80 @@ const Profile = () => {
       console.log("render content loaded");
       if (accountInfor) {
         return (
-          <div>
-            <h1>Account ID: {accountInfor.id} </h1>
+          <>
+            <Formik
+              initialValues={
+                {
+                  name: accountInfor.name,
+                  email: accountInfor.email,
+                  phone: accountInfor.phone,
+                }
+              }
+              validationSchema={UpdateProfileSchema}
+              onSubmit={updateProfile}
+            >
+              {({ isValid, isSubmitting, errors, touched }) => (
+                <Form>
+                  <div className="form-group">
+                    <label>Email:</label>
+                    <Field
+                      name="email"
+                      type="email"
+                      disabled={!isEditing}
+                      className={`form-control ${
+                        errors.email && touched.email ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="invalid-feedback custom-error-message"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Name:</label>
+                    <Field
+                      name="name"
+                      type="text"
+                      className={`form-control ${
+                        errors.name && touched.name ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      className="invalid-feedback custom-error-message"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Phone Number:</label>
+                    <Field
+                      name="phoneNumber"
+                      type="text"
+                      className={`form-control ${
+                        errors.email && touched.email ? "is-invalid" : ""
+                      }`}
+                    />
+                    <ErrorMessage
+                      name="phoneNumber"
+                      component="div"
+                      className="invalid-feedback custom-error-message"
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-primary btn-block" disabled={!isValid || isSubmitting || !isEditing}>Save</button>
+                  <button type="button" className="btn btn-primary btn-block" onClick={renderContent} disabled={!isEditing}>Canel</button>
+                  <button type="button" className="btn btn-primary btn-block" onClick={() =>{setIsEditing(true)}}>Edit</button>
+                </Form>
+              )}
+            </Formik>
+            {/* <h1>Account ID: {accountInfor.id} </h1>
             <h1>Account Name: {accountInfor.name}</h1>
             <h1>Account Email: {accountInfor.email}</h1>
             <h1>Account Phone: {accountInfor.phone}</h1>
             <h1>Account Avatar: {accountInfor.avatar}</h1>
             <h1>Account Role: {accountInfor.role}</h1>
-            <h1>Account Status: {accountInfor.status}</h1>
-          </div>
+            <h1>Account Status: {accountInfor.status}</h1> */}
+          </>
         );
       }
     }
@@ -62,13 +140,14 @@ const Profile = () => {
     navigate("/");
   };
 
+  const updateProfile = () => {};
+
   return (
     <>
       <div className="clearfix"></div>
       <div className="container">
         <div className="row display-flex">
           <div className="col-md-4">
-            {/* {token ? renderContent():"NULL"} */}
             <div className="profile-menu">
               <img
                 className="avatar-profile"
@@ -80,13 +159,15 @@ const Profile = () => {
                 <div className="item-menu">Liked Products</div>
                 <div className="item-menu">Orders</div>
                 <div className="item-menu">Change Password</div>
-                <div className="item-menu" onClick={logOut} >Log Out</div>
+                <div className="item-menu" onClick={logOut}>
+                  Log Out
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-8">
             <div className="infor-content content">
-              {token ? renderContent() : navigate("/")}
+              {renderContent()}
             </div>
           </div>
         </div>
