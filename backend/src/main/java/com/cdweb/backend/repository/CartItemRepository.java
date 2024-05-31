81dd25cfa129efc7df499ed96006e95619b09bab
BELCAO <caosybel@gmail.com>
2024-05-31 22:42:12 +0700

    fix_cart

--- backend/src/main/java/com/cdweb/backend/repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdweb.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cdweb.backend.entity.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
